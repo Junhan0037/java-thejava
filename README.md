f3b6ff88f359c9bc9a9535ee3218a08e871e1bbb
Junhan0037 <wnsgks0037@naver.com>
2020-10-18 15:26:49 +0900

    ADD : JVM 이해하기

---
+++ README.md
@@ -0,0 +1,9 @@
+# java-thejava ![Java_11](https://img.shields.io/badge/java-v11-red?logo=java)
+
+더 자바, 코드를 조작하는 다양한 방법
+
+## 목록
+
+#### 1. [JVM 이해하기](https://www.notion.so/JVM-a7a13d08c29c48a39189e15f759757f9)
+
+해당 repo는 [더 자바, 코드를 조작하는 다양한 방법 - 백기선](https://www.inflearn.com/course/the-java-code-manipulation) 해당 강의를 듣고 정리한 REPO 입니다.
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ package org.example;
 public class App {
 
     public static void main( String[] args ) {
-        ClassLoader classLoader = App.class.getClassLoader();
+        ClassLoader classLoader = App.class.getClassLoader(); // 클래스 로더
         System.out.println(classLoader);
         System.out.println(classLoader.getParent());
         System.out.println(classLoader.getParent().getParent());
